f2e25e51ff109953f8f12d645063e76bd6ffb487
Kirill N <k.nikolaenko@zebra-group.ru>
2017-03-08 18:09:16 +0300

    fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,6 @@
-$(".lazy").lazyload({
-	event : "carousel-show"
-});
+$(".lazy").lazyload();
 $('.carousel-inner .item').first().addClass('active');
 $("#instagram-carousel").on('slide.bs.carousel', function () {
 	var $itm = $('.item.active+.item img');
 	$itm.attr('src', $itm.attr('data-original'));
 });
-
